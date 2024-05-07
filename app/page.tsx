2263065b851220b82638f46c71b0d3cdda109987
JERROL-LAPTOP\miki <john.michael.vallejo@easycom-intl.com>
2024-05-07 11:26:24 +0800

    change landing content

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
 <main>
-  This is testing
+    <div>Title of the project: Sample</div>
+    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta earum rem dolores. Debitis mollitia rerum vel sint culpa. Sequi natus dolor explicabo animi aliquid voluptatum earum eius amet laborum temporibus?</div>
 </main>
   );
 }
